356cfbeb338a129719582f4589187af51b53e003
Ananth Murugan G K <ananthmgk4002@gmail.com>
2022-12-13 22:27:52 +0530

    added three background images with button.

--- main.js
+++ main.js
@@ -1,34 +1,62 @@
 window.addEventListener('load', () => {
-    const secondHand = document.querySelector('.second-hand');
-    const minutesHand = document.querySelector('.min-hand');
-    const hourHand = document.querySelector('.hour-hand');
+  const secondHand = document.querySelector('.second-hand');
+  const minutesHand = document.querySelector('.min-hand');
+  const hourHand = document.querySelector('.hour-hand');
+  const button = document.querySelector('.button');
+  const clockFace = document.querySelector('.clock-face');
 
-    const weekdayList = ["SUN","MON","TUE","WED","THU","FRI","SAT"];
-    const monthList = ["JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"];
+  const weekdayList = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+  const monthList = [
+    'JAN',
+    'FEB',
+    'MAR',
+    'APR',
+    'MAY',
+    'JUN',
+    'JUL',
+    'AUG',
+    'SEP',
+    'OCT',
+    'NOV',
+    'DEC',
+  ];
 
-    const now = new Date();
-    const day = weekdayList[now.getDay()];
-    const month = monthList[now.getMonth()];
-    const date = now.getDate();
+  const now = new Date();
+  const day = weekdayList[now.getDay()];
+  const month = monthList[now.getMonth()];
+  const date = now.getDate();
 
-    document.querySelector(".day-display").innerHTML = day;
-    document.querySelector(".month-display").innerHTML = month;
-    document.querySelector(".date-display").innerHTML = date;
+  button.addEventListener('click', () => {
+    if (clockFace.classList == 'clock-face') {
+      clockFace.classList.remove('clock-face');
+      clockFace.classList.add('clock-face2');
+    } else if (clockFace.classList == 'clock-face2') {
+      clockFace.classList.remove('clock-face2');
+      clockFace.classList.add('clock-face3');
+    } else {
+      clockFace.classList.remove('clock-face3');
+      clockFace.classList.add('clock-face');
+    }
+  });
 
-function setDate() {
+  document.querySelector('.day-display').innerHTML = day;
+  document.querySelector('.month-display').innerHTML = month;
+  document.querySelector('.date-display').innerHTML = date;
+
+  function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
     const minutes = now.getMinutes();
     const hour = now.getHours();
 
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
-    const minutesDegrees = ((minutes / 60) * 360) + ((seconds/60)*6) + 90;
-    const hourDegrees = ((hour / 12) * 360) + ((minutes/60)*30) + 90;
-    
+    const secondsDegrees = (seconds / 60) * 360 + 90;
+    const minutesDegrees = (minutes / 60) * 360 + (seconds / 60) * 6 + 90;
+    const hourDegrees = (hour / 12) * 360 + (minutes / 60) * 30 + 90;
+
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
     minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-};
+  }
 
-setInterval(setDate, 1000);
+  setInterval(setDate, 1000);
 });
